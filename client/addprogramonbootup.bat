60b38592186800b422774da02e46f788f5a8c570
jules1468 <85050795+jules1468@users.noreply.github.com>
2024-05-19 22:17:13 +0200

    Delete client directory

--- client/addprogramonbootup.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-start copy main.lnk "C:\Users\BAESL\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-timeout /t 1>nul
-start main.bat
-timeout /t 1>nul
-start exit
--- client/main.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start winvnc.exe -run
-timeout /t 1 >nul
-start winvnc.exe -connect 192.168.178.65::4444
